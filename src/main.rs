23d4705d9200935fae3d101091f0dcbc08db7b21
Paul Cockrell <paulcockrell@gmail.com>
2025-11-18 20:34:12 +0000

    Add motor control

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,38 @@ struct RobotState {
     last_cmd: String,
 }
 
+struct Motor {
+    in1: OutputPin,
+    in2: OutputPin,
+    en: OutputPin,
+}
+
+impl Motor {
+    pub fn new(in1: u8, in2: u8, en: u8) -> Result<Motor> {
+        let gpio = Gpio::new().context("Failed to initialize GPIO")?;
+        let in1 = gpio.get(in1)?.into_output_low();
+        let in2 = gpio.get(in2)?.into_output_low();
+        let mut en = gpio.get(en)?.into_output();
+        en.set_pwm_frequency(1000.0, 1.0)?;
+
+        Ok(Self { in1, in2, en })
+    }
+
+    pub fn forward(&mut self, speed: u8 /* 0..100 */) -> Result<()> {
+        self.in1.set_high();
+        self.in2.set_low();
+        let duty_cycle = speed as f64 / 100.0;
+        Ok(self.en.set_pwm_frequency(1000.0, duty_cycle)?)
+    }
+
+    pub fn backward(&mut self, speed: u8 /* 0..100 */) -> Result<()> {
+        self.in1.set_low();
+        self.in2.set_high();
+        let duty_cycle = speed as f64 / 100.0;
+        Ok(self.en.set_pwm_frequency(1000.0, duty_cycle)?)
+    }
+}
+
 struct UltrasoundSensor {
     trig: OutputPin,
     echo: InputPin,
@@ -181,7 +213,23 @@ async fn main() {
     }
 
     //
-    // Task D - Global robot logic loop
+    // Task D - Motor controller
+    {
+        let mut motor_1 = Motor::new(26, 21, 4).unwrap();
+        let speed = 100;
+
+        task::spawn_blocking(move || {
+            loop {
+                let _ = motor_1.forward(speed);
+                std::thread::sleep(Duration::from_millis(2000));
+                let _ = motor_1.backward(speed);
+                std::thread::sleep(Duration::from_millis(2000));
+            }
+        });
+    }
+
+    //
+    // Task E - Global robot logic loop
     //
     loop {
         let s = state.lock().unwrap();
