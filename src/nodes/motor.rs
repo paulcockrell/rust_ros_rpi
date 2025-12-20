8503cb344ad8619829738d7f6be77de81863d0d9
Paul Cockrell <paulcockrell@gmail.com>
2025-12-20 14:51:01 +0000

    Update motor node to use the new format for constructing actuator nodes

--- src/nodes/motor.rs
+++ src/nodes/motor.rs
@@ -1,45 +1,70 @@
+use std::{sync::mpsc, time::Duration};
+
 use crate::{
     bus::{
-        event::{Event, MotorDirection},
+        event::{Event, MotorCommand, MotorDirection},
         event_bus::EventBus,
     },
     hal::motor::Motor,
 };
 
 pub async fn run(bus: EventBus) {
-    let mut rx = bus.subscribe();
-    let mut left = Motor::new(26, 21, 4).unwrap();
-    let mut right = Motor::new(27, 18, 17).unwrap();
+    let mut bus_rx = bus.subscribe();
+
+    let (tx, rx) = mpsc::channel::<MotorCommand>();
+
+    let motor_task = tokio::task::spawn_blocking(move || {
+        let mut left = Motor::new(26, 21, 4).unwrap();
+        let mut right = Motor::new(27, 18, 17).unwrap();
+
+        loop {
+            match rx.recv_timeout(Duration::from_millis(50)) {
+                Ok(cmd) => match cmd.direction {
+                    MotorDirection::Forward => {
+                        let _ = left.forward(cmd.speed);
+                        let _ = right.forward(cmd.speed);
+                    }
+                    MotorDirection::Backward => {
+                        let _ = left.backward(cmd.speed);
+                        let _ = right.backward(cmd.speed);
+                    }
+                    MotorDirection::Left => {
+                        let _ = left.backward(cmd.speed);
+                        let _ = right.forward(cmd.speed);
+                    }
+                    MotorDirection::Right => {
+                        let _ = left.forward(cmd.speed);
+                        let _ = right.backward(cmd.speed);
+                    }
+                    MotorDirection::Stop => {
+                        let _ = left.forward(0);
+                        let _ = right.forward(0);
+                    }
+                },
+                Err(mpsc::RecvTimeoutError::Timeout) => {
+                    // idle tick, do nothing
+                }
+                Err(mpsc::RecvTimeoutError::Disconnected) => {
+                    break;
+                }
+            }
+        }
+    });
 
     loop {
-        match rx.recv().await {
-            Ok(Event::MotorCommand(cmd)) => match cmd.direction {
-                MotorDirection::Forward => {
-                    let _ = left.forward(cmd.speed);
-                    let _ = right.forward(cmd.speed);
-                }
-                MotorDirection::Backward => {
-                    let _ = left.backward(cmd.speed);
-                    let _ = right.backward(cmd.speed);
-                }
-                MotorDirection::Left => {
-                    let _ = left.backward(cmd.speed);
-                    let _ = right.forward(cmd.speed);
-                }
-                MotorDirection::Right => {
-                    let _ = left.forward(cmd.speed);
-                    let _ = right.backward(cmd.speed);
-                }
-                MotorDirection::Stop => {
-                    let _ = left.forward(0);
-                    let _ = right.forward(0);
-                }
-            },
+        match bus_rx.recv().await {
+            Ok(Event::MotorCommand(cmd)) => {
+                let _ = tx.send(cmd);
+            }
             Ok(Event::Shutdown) => {
                 println!("Motor node shutting down");
                 break;
             }
+            Err(_) => break,
             _ => {}
         }
     }
+
+    drop(tx);
+    let _ = motor_task.await;
 }
