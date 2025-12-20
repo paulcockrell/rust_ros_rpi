4ded9ee30591474c066f784313ddb80aa25d1304
Paul Cockrell <paulcockrell@gmail.com>
2025-12-20 18:19:47 +0000

    Fixup camera node. that was the last one

--- src/nodes/camera.rs
+++ src/nodes/camera.rs
@@ -2,27 +2,40 @@ use crate::{
     bus::{event::Event, event_bus::EventBus},
     hal::camera::Camera,
 };
-use tokio::time::Duration;
+use std::{
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    time::Duration,
+};
 
 pub async fn run(bus: EventBus) {
-    let mut rx = bus.subscribe();
-    let mut camera = Camera::new().expect("Could not setup camera");
+    let mut bus_rx = bus.subscribe();
 
-    loop {
-        tokio::select! {
-            _ = tokio::time::sleep(Duration::from_secs(1))=>{
-                if matches!(camera.save_frame(), Ok(true)) {
-                    println!("Saved frame");
-                } else {
-                    println!("Failed to save frame");
-                }
+    let running = Arc::new(AtomicBool::new(true));
+    let running_thread = running.clone();
+
+    let task = tokio::task::spawn_blocking(move || {
+        let mut camera = Camera::new().expect("Could not setup camera");
+        while running_thread.load(Ordering::Relaxed) {
+            if matches!(camera.save_frame(), Ok(true)) {
+                println!("Saved frame");
+            } else {
+                println!("Failed to save frame");
             }
-            msg = rx.recv()=>{
-                if matches!(msg, Ok(Event::Shutdown)) {
-                    println!("Camera node shutting down");
-                    break;
-                }
-            }
+
+            std::thread::sleep(Duration::from_millis(1000));
+        }
+    });
+
+    while let Ok(event) = bus_rx.recv().await {
+        if matches!(event, Event::Shutdown) {
+            println!("LDR node shutting down");
+            break;
         }
     }
+
+    running.store(false, Ordering::Relaxed);
+    let _ = task.await;
 }
